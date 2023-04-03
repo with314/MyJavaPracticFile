3772b899f98615fd35c7dde3c9d2edcf1b40a065
An <81519430+with314@users.noreply.github.com>
2023-04-03 21:46:04 +0900

    JavaPractice
    練習です

---
+++ Practicjava.java
@@ -0,0 +1,18 @@
+class Circle{
+	public static void main(String[] args){
+	 int radius = Integer.parseInt(args[0]);
+	 double pi = 3.141592;
+     double area;
+   
+   area = radius * radius * pi;
+   
+   area = (int)(area * 100);
+   area = area / 100;
+		
+		System.out.println("hannkei : " + radius);
+		System.out.println("mennseki : " + area);
+
+   
+  
+  }
+}
